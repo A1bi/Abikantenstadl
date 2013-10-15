1c337e795abcf042cbf5b226703651a743c8ed9a
Albrecht Oster <a.oster@online.de>
2013-10-15 22:12:53 +0200

    initial commit

---
+++ config/deploy.rb
@@ -0,0 +1,28 @@
+require "bundler/capistrano"
+
+load "config/recipes/base"
+load "config/recipes/nginx"
+load "config/recipes/unicorn"
+load "config/recipes/mysql"
+load "config/recipes/memcached"
+load "config/recipes/rails"
+
+server "85.214.76.70", :web, :app, :db, primary: true
+
+set :user, "deployer"
+set :application, "Abikantenstadl"
+set :github_user, "A1bi"
+set :domain_name, "abikantenstadl.net"
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+
+set :scm, "git"
+set :repository, "git@github.com:#{github_user}/#{application}.git"
+set :branch, "master"
+set :git_enable_submodules, 1
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
